b1e2c45b5292cf406d632b82983448c859eb049a
Esau Silva <esausilva85@gmail.com>
2018-06-19 09:52:20 -0500

    Added production support, deployment to Heroku

--- README.md
+++ README.md
@@ -24,6 +24,12 @@ To start the server and client at the same time (from the root of the project)
 yarn dev
 ```
 
+Running the production build on localhost. This will create a production build, then Node will serve the app on http://localhost:5000
+
+```
+NODE_ENV=production yarn dev:server
+```
+
 ## How this works
 
 The key to use an Express backend with a project created with `create-react-app` is on using a **proxy**. We have a _proxy_ entry in `client/package.json`
--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+// API calls
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
 });
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
