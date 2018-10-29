aed7bbffde41514be3cdfecc1539e0f1b532a3ae
Esau Silva <esausilva85@gmail.com>
2018-10-29 08:40:09 -0500

    Added POST

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,8 @@ import './App.css';
 class App extends Component {
   state = {
     response: '',
+    post: '',
+    responseToPost: '',
   };
 
   componentDidMount() {
@@ -22,6 +24,20 @@ class App extends Component {
     if (response.status !== 200) throw Error(body.message);
 
     return body;
+  };
+
+  handleSubmit = async e => {
+    e.preventDefault();
+    const response = await fetch('/api/world', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ post: this.state.post }),
+    });
+    const body = await response.text();
+
+    this.setState({ responseToPost: body });
   };
 
   render() {
@@ -42,6 +58,18 @@ class App extends Component {
           </a>
         </header>
         <p>{this.state.response}</p>
+        <form onSubmit={this.handleSubmit}>
+          <p>
+            <strong>Post to Server:</strong>
+          </p>
+          <input
+            type="text"
+            value={this.state.post}
+            onChange={e => this.setState({ post: e.target.value })}
+          />
+          <button type="submit">Submit</button>
+        </form>
+        <p>{this.state.responseToPost}</p>
       </div>
     );
   }
--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // API calls
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
+});
+
+app.post('/api/world', (req, res) => {
+  console.log(req.body);
+  res.send(
+    `I received your POST request. This is what you sent me: ${req.body.post}`,
+  );
 });
 
 if (process.env.NODE_ENV === 'production') {
